f26dde99920be13151c50b0311ab82a2b782a4dd
Hak1e <hakie.notiko@gmail.com>
2023-07-11 21:28:25 +0400

    Новые столбцы в таблицах БД

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,25 @@
 DROP TABLE IF EXISTS guild_settings;
+DROP TABLE IF EXISTS cooldowns;
+
 
 CREATE TABLE IF NOT EXISTS guild_settings (
     guild_id BIGINT PRIMARY KEY,
     art_channel_id BIGINT,
     meme_channel_id BIGINT,
-    temp_voice_channel_category_id BIGINT,
-    temp_create_voice_channel BIGINT
+
+    text_channel_category_id BIGINT,
+    roles_id_to_mention BIGINT[],
+
+    voice_channel_category_id BIGINT,
+    create_voice_channel_id BIGINT,
+    created_voice_channel_ids BIGINT[],
+
+    button_cooldown INT
 
 );
 
-alter table guild_settings
-    add temp_voice_channel_id BIGINT[];
 
-
+CREATE TABLE IF NOT EXISTS cooldowns (
+    user_id BIGINT PRIMARY KEY,
+    button_cooldown_end_time TIMESTAMP WITH TIME ZONE
+);
