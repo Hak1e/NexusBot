00aae22908417396e993f5e46c03cc30515b9f0b
Hak1e <hakie.notiko@gmail.com>
2023-08-12 16:43:04 +0400

    Изменён стиль кода

--- cogs/temp_voice_channel.py
+++ cogs/temp_voice_channel.py
@@ -18,30 +18,32 @@ class OnJoinChannel(commands.Cog):
         query = "SELECT channel_name " \
                 "FROM custom_voice " \
                 "WHERE guild_id = $1 and user_id = $2"
-        self.custom_channel_name = await self.pool.fetchval(query, member.guild.id, member.id)
+        self.custom_channel_name = await self.pool.fetchval(query, member.guild.id,
+                                                            member.id)
 
         category = member.guild.get_channel(self.category_id)
         channel_name = self.custom_channel_name or f"{member.name}'s channel"
 
-        voice_channel = await member.guild.create_voice_channel(
-            name=channel_name, category=category, overwrites=category.overwrites
-        )
+        voice_channel = await member.guild.create_voice_channel(name=channel_name, category=category,
+                                                                overwrites=category.overwrites)
         query = "INSERT INTO custom_voice (guild_id, user_id, channel_id)" \
                 "VALUES ($1, $2, $3)" \
                 "ON CONFLICT (guild_id, user_id) DO UPDATE " \
                 "SET channel_id = $3"
-        await self.pool.execute(query, member.guild.id, member.id, voice_channel.id)
+        await self.pool.execute(query, member.guild.id,
+                                member.id, voice_channel.id)
         category_ow = category.overwrites
         await asyncio.sleep(0.7)  # без этого права пользователю могут не добавиться
         # Значение 0.7 - минимальное для корректной работы
 
         if member in category_ow:
-            category_ow[member].update(
-                view_channel=True, manage_channels=True, manage_permissions=True, move_members=True)
+            category_ow[member].update(view_channel=True, manage_channels=True,
+                                       manage_permissions=True, move_members=True)
             await voice_channel.edit(overwrites=category_ow)
         else:
-            await voice_channel.set_permissions(
-                member, view_channel=True, manage_channels=True, manage_permissions=True, move_members=True)
+            await voice_channel.set_permissions(member, view_channel=True,
+                                                manage_channels=True, manage_permissions=True,
+                                                move_members=True)
 
         try:
             await member.move_to(voice_channel)
@@ -54,22 +56,24 @@ class OnJoinChannel(commands.Cog):
         query = "SELECT user_id " \
                 "FROM custom_voice " \
                 "WHERE guild_id = $1 and channel_id = $2"
-        self.channel_author_id = await self.pool.fetchval(query, member.guild.id, channel.id)
+        self.channel_author_id = await self.pool.fetchval(query, member.guild.id,
+                                                          channel.id)
 
         query = "INSERT INTO custom_voice (guild_id, user_id, channel_id, channel_name)" \
                 "VALUES ($1, $2, $3, $4)" \
                 "ON CONFLICT (guild_id, user_id) DO UPDATE " \
                 "SET channel_id = $3, channel_name = $4"
-        await self.pool.execute(query, member.guild.id, self.channel_author_id, None, channel.name)
+        await self.pool.execute(query, member.guild.id,
+                                self.channel_author_id, None,
+                                channel.name)
 
         self.channel_author_id = None
         await channel.delete()
         return
 
     @commands.Cog.listener()
-    async def on_voice_state_update(
-            self, member: disnake.Member, before: disnake.VoiceState, current: disnake.VoiceState
-    ):
+    async def on_voice_state_update(self, member: disnake.Member, before: disnake.VoiceState,
+                                    current: disnake.VoiceState):
         if before.channel == current.channel:
             return
 
