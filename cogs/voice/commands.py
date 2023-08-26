f67d432f73f8c0e4def570d9a66f85521e0a6f04
Hak1e <hakie.notiko@gmail.com>
2023-08-26 16:43:49 +0400

    Перенос команд, связанных с голосовыми каналами

---
+++ cogs/voice/commands.py
@@ -0,0 +1,48 @@
+import disnake
+from disnake.ext import commands
+import asyncpg
+from core.bot import Nexus
+
+
+class PingMembersInVoice(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.slash_command()
+    async def event_members(self, ctx: disnake.CommandInteraction,
+                            bounty: int = None):
+        """Оповестить всех, кто находится в голосовом канале с Вами
+
+        Parameters
+        ----------
+        ctx: command interactions
+        bounty: Валюта сервера
+        """
+        voice_channel_id = ctx.author.voice.channel.id
+        voice_channel = self.bot.get_channel(voice_channel_id)
+        members = voice_channel.members
+        add_money_to_member = [f".add-money `<@{member.id}>` {bounty}" for member in members]
+        members_names = [f"<@{member.id}>" for member in members]
+        embed = (
+            disnake.Embed(
+                description=f"**Участники канала** <#{voice_channel_id}>"
+            )
+            .set_footer(
+                text=f"Запрошено пользователем {ctx.author}",
+                icon_url=ctx.author.avatar.url
+            )
+            .add_field(
+                name="",
+                value="\n".join(members_names)
+            )
+            .add_field(
+                name="Команда для начисления кристаллов",
+                value="\n".join(add_money_to_member),
+                inline=False
+            )
+        )
+        await ctx.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(PingMembersInVoice(bot))
