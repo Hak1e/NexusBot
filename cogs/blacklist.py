f0110f25ceb6b371f2e3fbd19301677afce417fe
Hak1e <hakie.notiko@gmail.com>
2023-08-26 16:38:36 +0400

    Рефакторинг

--- cogs/blacklist.py
+++ cogs/blacklist.py
@@ -1,60 +1,9 @@
 import disnake
 from disnake.ext import commands
 from core.bot import Nexus
+from models.button_view import PageButtons
 import asyncpg
 from typing import Optional
-
-
-class TournamentButtons(disnake.ui.View):
-    def __init__(self, pages, timeout=60):
-        super().__init__(timeout=timeout)
-        self.pages = pages
-        self.current_page = 0
-
-    async def on_timeout(self) -> None:
-        self.stop()
-
-    @disnake.ui.button(label="Назад", custom_id="previous_page", style=disnake.ButtonStyle.blurple)
-    async def _previous_page(self, button: disnake.ui.Button, ctx: disnake.MessageInteraction):
-        if self.current_page > 0:
-            self.current_page -= 1
-            await ctx.response.edit_message(embed=self.pages[self.current_page])
-        else:
-            await ctx.response.defer()
-
-    @disnake.ui.button(label="Вперёд", custom_id="next_page", style=disnake.ButtonStyle.blurple)
-    async def _next_page(self, button: disnake.ui.Button, ctx: disnake.MessageInteraction):
-        if self.current_page < len(self.pages) - 1:
-            self.current_page += 1
-            await ctx.response.edit_message(embed=self.pages[self.current_page])
-        else:
-            await ctx.response.defer()
-
-    @disnake.ui.button(label="Закрыть", custom_id="close", style=disnake.ButtonStyle.red)
-    async def _close(self, button: disnake.ui.Button, ctx: disnake.MessageInteraction):
-        disabled_buttons = [
-            disnake.ui.Button(
-                label="Назад",
-                style=disnake.ButtonStyle.blurple,
-                custom_id="previous_page",
-                disabled=True
-            ),
-            disnake.ui.Button(
-                label="Вперёд",
-                style=disnake.ButtonStyle.blurple,
-                custom_id="next_page",
-                disabled=True
-            ),
-            disnake.ui.Button(
-                label="Закрыть",
-                style=disnake.ButtonStyle.red,
-                custom_id="close",
-                disabled=True
-            )
-        ]
-        new_embed = disnake.Embed(title="Чёрный список турнира", description="Список закрыт")
-        await ctx.response.edit_message(embed=new_embed, components=disabled_buttons)
-        self.stop()
 
 
 class Tournament(commands.Cog):
@@ -71,8 +20,10 @@ class Tournament(commands.Cog):
         pass
 
     @blacklist.sub_command()
-    async def add(self, ctx: disnake.CommandInteraction, member_or_id: disnake.User, reason: str):
+    async def add(self, ctx: disnake.CommandInteraction,
+                  member_or_id: disnake.User, reason: str):
         """Добавить участника в чёрный список
+
         Parameters
         ----------
         ctx: disnake.CommandInteraction
@@ -83,12 +34,15 @@ class Tournament(commands.Cog):
                 "VALUES ($1, $2, $3)" \
                 "ON CONFLICT (guild_id, user_id) DO UPDATE " \
                 "SET reason = $3"
-        await self.pool.execute(query, ctx.guild.id, member_or_id.id, reason)
-        await ctx.send(f"{member_or_id.name} добавлен в чёрный список", ephemeral=True)
+        await self.pool.execute(query, ctx.guild.id,
+                                member_or_id.id, reason)
+        await ctx.send(f"{member_or_id.mention} добавлен в чёрный список", ephemeral=True)
 
     @blacklist.sub_command()
-    async def remove(self, ctx: disnake.CommandInteraction, member_or_id: disnake.User):
+    async def remove(self, ctx: disnake.CommandInteraction,
+                     member_or_id: disnake.User):
         """Убрать участника из чёрного списка
+
         Parameters
         ----------
         ctx: disnake.CommandInteraction
@@ -96,12 +50,15 @@ class Tournament(commands.Cog):
         """
         query = "DELETE FROM tournament_blacklist " \
                 "WHERE guild_id = $1 and user_id = $2"
-        await self.pool.execute(query, ctx.guild.id, member_or_id.id)
+        await self.pool.execute(query, ctx.guild.id,
+                                member_or_id.id)
         await ctx.send(f"{member_or_id.name} удалён из чёрного списка", ephemeral=True)
 
     @blacklist.sub_command()
-    async def show(self, ctx: disnake.CommandInteraction, ephemeral: Optional[bool] = True):
+    async def show(self, ctx: disnake.CommandInteraction,
+                   ephemeral: Optional[bool] = True):
         """Показать чёрный список
+
         Parameters
         ----------
         ctx: disnake.CommandInteraction
@@ -111,6 +68,10 @@ class Tournament(commands.Cog):
                 "FROM tournament_blacklist " \
                 "WHERE guild_id = $1"
         blacklist = await self.pool.fetch(query, ctx.guild.id)
+        if not blacklist:
+            empty_embed = disnake.Embed(title="Чёрный список турнира", description="Список пуст")
+            await ctx.send(embed=empty_embed, ephemeral=ephemeral)
+            return
 
         users = [f"<@{value[0]}> (`{value[0]}`)" for value in blacklist]
         reasons = [value[1] for value in blacklist]
@@ -118,24 +79,20 @@ class Tournament(commands.Cog):
         pages = []
         items_per_page = 10
         counter = 1
-        for i in range(0, len(users), items_per_page):
-            page_users = users[i:i+items_per_page]
-            page_reasons = reasons[i:i+items_per_page]
+        for item_index in range(0, len(users), items_per_page):
+            page_users = users[item_index:item_index + items_per_page]
+            page_reasons = reasons[item_index:item_index + items_per_page]
             page = disnake.Embed(title="Чёрный список турнира")
             for user, reason in zip(page_users, page_reasons):
-                page.add_field(f"", f"{counter}) {user}\n{reason}", inline=False)
+                page.add_field(name=f"", value=f"{counter}) {user}\n{reason}",
+                               inline=False)
                 counter += 1
             pages.append(page)
 
-        buttons = TournamentButtons(pages)
-        if len(pages) > 0:
-            await ctx.send(embed=pages[0], view=buttons, ephemeral=ephemeral)
-        else:
-            empty_embed = disnake.Embed(title="Чёрный список турнира", description="Список пуст")
-            await ctx.send(embed=empty_embed, ephemeral=ephemeral)
-
+        buttons = PageButtons(pages)
+        await ctx.send(embed=pages[0], view=buttons,
+                       ephemeral=ephemeral)
 
 
 def setup(bot):
     bot.add_cog(Tournament(bot))
-
