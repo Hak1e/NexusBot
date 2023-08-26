92b2cf7a0ba876b711c9064e49e1eca386e63e4c
Hak1e <hakie.notiko@gmail.com>
2023-08-26 16:53:21 +0400

    Добавлена модель с кнопками для перелистывания страниц

---
+++ models/button_view.py
@@ -0,0 +1,55 @@
+import disnake
+from disnake.ext import commands
+
+
+class PageButtons(disnake.ui.View):
+    def __init__(self, pages, timeout=60):
+        super().__init__(timeout=timeout)
+        self.pages = pages
+        self.current_page = 0
+
+    async def on_timeout(self) -> None:
+        self.stop()
+
+    @disnake.ui.button(label="Назад", custom_id="previous_page", style=disnake.ButtonStyle.blurple)
+    async def _previous_page(self, button: disnake.ui.Button, ctx: disnake.MessageInteraction):
+        if self.current_page > 0:
+            self.current_page -= 1
+            await ctx.response.edit_message(embed=self.pages[self.current_page])
+        else:
+            await ctx.response.defer()
+
+    @disnake.ui.button(label="Вперёд", custom_id="next_page", style=disnake.ButtonStyle.blurple)
+    async def _next_page(self, button: disnake.ui.Button, ctx: disnake.MessageInteraction):
+        if self.current_page < len(self.pages) - 1:
+            self.current_page += 1
+            await ctx.response.edit_message(embed=self.pages[self.current_page])
+        else:
+            await ctx.response.defer()
+
+    @disnake.ui.button(label="Закрыть", custom_id="close", style=disnake.ButtonStyle.red)
+    async def _close(self, button: disnake.ui.Button, ctx: disnake.MessageInteraction):
+        disabled_buttons = [
+            disnake.ui.Button(
+                label="Назад",
+                style=disnake.ButtonStyle.blurple,
+                custom_id="previous_page",
+                disabled=True
+            ),
+            disnake.ui.Button(
+                label="Вперёд",
+                style=disnake.ButtonStyle.blurple,
+                custom_id="next_page",
+                disabled=True
+            ),
+            disnake.ui.Button(
+                label="Закрыть",
+                style=disnake.ButtonStyle.red,
+                custom_id="close",
+                disabled=True
+            )
+        ]
+        new_embed = disnake.Embed(title="", description="Список закрыт")
+        await ctx.response.edit_message(embed=new_embed, components=disabled_buttons)
+        self.stop()
+
