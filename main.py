d1ca0877091619788207410006f0b439bbe26938
Hak1e <hakie.notiko@gmail.com>
2023-09-10 13:59:58 +0400

    Изменена загрузка когов

--- main.py
+++ main.py
@@ -3,12 +3,24 @@ import os
 import asyncio
 
 
+async def load_cogs(bot, root_directory):
+    bot.load_extensions("./cogs/")
+    for root, directories, files in os.walk(root_directory):
+        for directory in directories:
+            if directory == "__pycache__":
+                print(f"Pycache skipped")
+                continue
+            try:
+                bot.load_extensions(f"{root_directory}{directory}/")
+            except Exception as e:
+                print(f"Load error: {e}")
+
+
 async def main():
     bot = Nexus()
     await bot.connect_to_db()
-    bot.load_extensions("./cogs/")
+    await load_cogs(bot, "./cogs/")
     await bot.start(token=os.getenv("TOKEN"))
 
 if __name__ == "__main__":
     asyncio.run(main())
-
