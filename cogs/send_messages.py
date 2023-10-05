14811b9d60665704d48015007205098b144c001c
Hak1e <hakie.notiko@gmail.com>
2023-10-05 16:32:38 +0400

    Добавлено прослушивание и отправка сообщений с файлами от пользователя в чат

--- cogs/send_messages.py
+++ cogs/send_messages.py
@@ -24,6 +24,59 @@ class SendMessages(commands.Cog):
         await channel.send(message)
         await ctx.send("Сообщение отправлено", ephemeral=True)
 
+    async def wait_for_message(self, ctx):
+        def check(msg):
+            return msg.author.id == ctx.author.id and msg.channel == ctx.channel
+
+        return await self.bot.wait_for("message", check=check)
+
+    @commands.slash_command()
+    async def echo(self, ctx: disnake.CommandInteraction,
+                   channel_id, guild_id=None):
+        """Начать слушать сообщения пользователя и отправлять их в указанный чат
+
+        Parameters
+        ----------
+        ctx: command interaction
+        channel_id: Указать ID канала для отправки сообщения
+        guild_id: Указать ID сервера. Указывать, если канал на другом сервере
+        """
+        accepted_person_ids = [389787190986670082, 434780487127400459]
+        if ctx.author.id not in accepted_person_ids:
+            await ctx.send("Вам не разрешено использовать данную команду", ephemeral=True)
+            return
+
+        try:
+            guild = ctx.bot.get_guild(int(guild_id)) if guild_id else ctx.guild
+            if not guild:
+                raise ValueError
+        except:
+            await ctx.send("Не найден указанный сервер", ephemeral=True)
+            return
+        try:
+            channel = guild.get_channel(int(channel_id))
+            if not channel:
+                raise ValueError
+        except:
+            await ctx.send("Не найден канал на этом сервере", ephemeral=True)
+            return
+
+        await ctx.send("Начните вводить сообщения, которые будут отправлены в указанный канал\n"
+                       "Чтобы остановить команду, введите `<<stop`", ephemeral=True)
+        while True:
+            message = await self.wait_for_message(ctx)
+            if message.content == "<<stop":
+                await message.delete()
+                await ctx.send("Отправка сообщений остановлена", ephemeral=True)
+                break
+
+            if message.attachments:
+                attachments = [await attachment.to_file() for attachment in message.attachments]
+                await channel.send(message.content, files=attachments)
+            else:
+                await channel.send(message.content)
+            await message.delete()
+
 
 def setup(bot):
     bot.add_cog(SendMessages(bot))
