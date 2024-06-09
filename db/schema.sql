f30f868c8c0c051c99e0ae0caaf171d17cc35c3a
Hak1e <hakie.notiko@gmail.com>
2024-06-09 21:32:33 +0400

    Крупное обновление базы данных

--- db/schema.sql
+++ db/schema.sql
@@ -1,35 +1,115 @@
-CREATE TABLE IF NOT EXISTS guild_settings (
-    guild_id BIGINT PRIMARY KEY,
-    tickets_category_id BIGINT,
-    voice_channel_category_id BIGINT,
-    channel_creator_id BIGINT
+CREATE TABLE IF NOT EXISTS guild (
+    id BIGINT PRIMARY KEY,
+    owner_id BIGINT
 );
 
-CREATE TABLE IF NOT EXISTS text_channels (
-    guild_id BIGINT PRIMARY KEY,
-    art_channel_id BIGINT,
-    meme_channel_id BIGINT,
-    roles_id_to_mention BIGINT[],
-    goodbye_channel_id BIGINT
+CREATE TABLE IF NOT EXISTS guild_sync (
+    guild_id BIGINT REFERENCES guild(id) PRIMARY KEY,
+    member_name BOOLEAN DEFAULT FALSE,
+    member_roles BOOLEAN DEFAULT FALSE
 );
 
-CREATE TABLE IF NOT EXISTS emoji_reactions (
-    guild_id BIGINT PRIMARY KEY,
-    _like TEXT,
-    _dislike TEXT
+CREATE TABLE IF NOT EXISTS guild_member (
+    id BIGINT UNIQUE,
+    guild_id BIGINT REFERENCES guild(id),
+    nick TEXT,
+    PRIMARY KEY (id, guild_id)
 );
 
-CREATE TABLE IF NOT EXISTS cooldown (
-    guild_id BIGINT PRIMARY KEY,
-    user_id BIGINT,
-    button_cooldown INT,
-    button_cooldown_end_time TIMESTAMP WITH TIME ZONE
+CREATE TABLE IF NOT EXISTS role (
+    id BIGINT PRIMARY KEY,
+    guild_id BIGINT REFERENCES guild(id),
+    name TEXT
 );
 
-CREATE TABLE IF NOT EXISTS custom_voice (
-    guild_id BIGINT,
-    user_id BIGINT,
-    channel_id BIGINT,
+CREATE TABLE IF NOT EXISTS member_role (
+    member_id BIGINT REFERENCES guild_member(id),
+    guild_id BIGINT REFERENCES guild(id),
+    role_id BIGINT REFERENCES role(id),
+    PRIMARY KEY (member_id, guild_id, role_id)
+);
+
+CREATE TABLE IF NOT EXISTS goodbye_channel (
+    id BIGINT PRIMARY KEY,
+    guild_id BIGINT REFERENCES guild(id)
+);
+
+CREATE TABLE IF NOT EXISTS art_channel (
+    id BIGINT PRIMARY KEY,
+    guild_id BIGINT REFERENCES guild(id)
+);
+
+CREATE TABLE IF NOT EXISTS meme_channel (
+    id BIGINT PRIMARY KEY,
+    guild_id BIGINT REFERENCES guild(id)
+);
+
+CREATE TABLE IF NOT EXISTS emoji_reaction (
+    guild_id BIGINT REFERENCES guild(id) PRIMARY KEY,
+    "like" TEXT,
+    dislike TEXT
+);
+
+CREATE TABLE IF NOT EXISTS tournament_blacklist (
+    guild_id BIGINT REFERENCES guild(id),
+    user_id BIGINT REFERENCES guild_member(id),
+    reason TEXT,
+    PRIMARY KEY (guild_id, user_id)
+);
+
+CREATE TABLE IF NOT EXISTS journal (
+    guild_id BIGINT REFERENCES guild(id),
+    user_id BIGINT REFERENCES guild_member(id),
+    notes TEXT[],
+    PRIMARY KEY (guild_id, user_id)
+);
+
+CREATE TABLE IF NOT EXISTS journal_log_channel (
+    id BIGINT PRIMARY KEY,
+    guild_id BIGINT REFERENCES guild(id)
+);
+
+
+CREATE TABLE IF NOT EXISTS user_role (
+    user_id BIGINT REFERENCES guild_member(id),
+    guild_id BIGINT REFERENCES guild(id),
+    role_id BIGINT REFERENCES role(id),
+    PRIMARY KEY (user_id, guild_id, role_id)
+);
+
+CREATE TABLE IF NOT EXISTS bot_author (
+    user_id BIGINT UNIQUE REFERENCES guild_member(id)
+);
+
+-- CREATE TABLE IF NOT EXISTS error_logs_channel (
+--     id BIGINT PRIMARY KEY,
+--     guild_id BIGINT REFERENCES guild(id)
+-- );
+
+
+-- region Lobbies
+
+CREATE TABLE IF NOT EXISTS lobby_voice_channel_creator_settings (
+    id BIGINT PRIMARY KEY,
+    guild_id BIGINT REFERENCES guild(id),
+    category_id_for_new_channel BIGINT,
+    user_limit INT,
+    custom BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE IF NOT EXISTS lobby_voice_channel_creator_role (
+    voice_channel_id BIGINT REFERENCES lobby_voice_channel_creator_settings(id),
+    role_id BIGINT REFERENCES role(id),
+    guild_id BIGINT REFERENCES guild(id),
+    role_needed BOOL DEFAULT FALSE,
+    default_name TEXT,
+    role_not_found_message TEXT,
+    PRIMARY KEY (voice_channel_id, role_id)
+);
+
+CREATE TABLE IF NOT EXISTS lobby_voice_channel_settings (
+    guild_id BIGINT REFERENCES guild(id),
+    user_id BIGINT REFERENCES guild_member(id),
     channel_name TEXT,
     bitrate INT,
     user_limit INT,
@@ -37,123 +117,105 @@ CREATE TABLE IF NOT EXISTS custom_voice (
     PRIMARY KEY (guild_id, user_id)
 );
 
-CREATE TABLE IF NOT EXISTS tournament_blacklist (
-    guild_id BIGINT,
-    user_id BIGINT,
-    reason TEXT,
-    PRIMARY KEY (guild_id, user_id)
+CREATE TABLE IF NOT EXISTS lobby_voice_channel_author (
+    voice_channel_id BIGINT PRIMARY KEY,
+    guild_id BIGINT REFERENCES guild(id),
+    user_id BIGINT REFERENCES guild_member(id)
 );
 
-CREATE TABLE IF NOT EXISTS journal (
-    guild_id BIGINT,
-    user_id BIGINT,
-    notes TEXT[],
-    PRIMARY KEY (guild_id, user_id)
+CREATE TABLE IF NOT EXISTS lobby_text_channel (
+    id BIGINT,
+    voice_channel_creator_id BIGINT REFERENCES lobby_voice_channel_creator_settings(id),
+    guild_id BIGINT REFERENCES guild(id),
+    PRIMARY KEY (id, voice_channel_creator_id)
 );
 
-CREATE TABLE IF NOT EXISTS journal_logs (
-    guild_id BIGINT PRIMARY KEY,
-    channel_id BIGINT
-);
-
-CREATE TABLE IF NOT EXISTS roles (
-    role_id BIGINT PRIMARY KEY,
-    guild_id BIGINT,
-    name TEXT
-);
-
-CREATE TABLE IF NOT EXISTS user_roles (
-    id SERIAL PRIMARY KEY,
-    guild_id BIGINT,
-    role_id BIGINT REFERENCES roles(role_id)
-);
-
-CREATE TABLE IF NOT EXISTS bot_author (
-    num SERIAL PRIMARY KEY,
-    user_id BIGINT
-);
-
-insert into bot_author (user_id) values (389787190986670082);
-
-CREATE TABLE IF NOT EXISTS error_logs_channel (
-    guild_id BIGINT PRIMARY KEY,
-    channel_id BIGINT
-);
-
--- region Lobbies
-alter table voice_creators rename to lobby_voice_creators;
-alter table category_rank_roles rename to lobby_category_rank_roles;
-alter table rating_lobby_text_channel_id rename to lobby_text_channel_ids;
-alter table created_lobbies_category_id rename to lobby_created_voice_channels_ids;
-
-CREATE TABLE IF NOT EXISTS lobby_voice_creators (
-    guild_id BIGINT,
-    channel_creator_id BIGINT,
-    user_limit INT,
-    category_id BIGINT,
-    PRIMARY KEY (guild_id, channel_creator_id)
-);
-
-drop table lobby_category_rank_roles;
-CREATE TABLE IF NOT EXISTS lobby_category_rank_roles (
-    guild_id BIGINT,
-    category_id BIGINT,
-    role_id BIGINT,
-    role_name TEXT,
-    role_needed BOOL DEFAULT FALSE,
-    PRIMARY KEY (guild_id, category_id, role_id)
-);
-alter table lobby_category_rank_roles add role_needed BOOL DEFAULT FALSE;
-
-CREATE TABLE IF NOT EXISTS lobby_text_channel_ids (
-    guild_id BIGINT,
-    lobby_category_id BIGINT,
+CREATE TABLE IF NOT EXISTS lobby_message (
+    id BIGINT,
+    voice_channel_id BIGINT REFERENCES lobby_voice_channel_author(voice_channel_id),
+    guild_id BIGINT REFERENCES guild(id),
     text_channel_id BIGINT,
-    PRIMARY KEY (guild_id, lobby_category_id)
+    PRIMARY KEY (id, voice_channel_id)
 );
 
-CREATE TABLE IF NOT EXISTS lobby_messages (
-    guild_id BIGINT,
-    message_id BIGINT,
-    voice_channel_id BIGINT,
-    PRIMARY KEY (guild_id, message_id)
-);
+-- CREATE TABLE IF NOT EXISTS lobby_voice_creators (
+--     guild_id BIGINT REFERENCES guild(id),
+--     channel_creator_id BIGINT,
+--     user_limit INT,
+--     category_id BIGINT,
+--     PRIMARY KEY (guild_id, channel_creator_id)
+-- );
 
-CREATE TABLE IF NOT EXISTS lobby_created_voice_channels_ids (
-    guild_id BIGINT,
-    channel_creator_category_id BIGINT,
-    lobby_category_id BIGINT,
-    PRIMARY KEY (guild_id, channel_creator_category_id)
-);
+-- alter table lobby_category_rank_roles rename to lobby_category_roles;
+-- alter table lobby_category_rank_roles add default_name TEXT DEFAULT NULL;
+-- alter table lobby_category_rank_roles add role_not_found_message TEXT DEFAULT NULL;
+-- CREATE TABLE IF NOT EXISTS lobby_category_rank_roles (
+--     guild_id BIGINT REFERENCES guild(id),
+--     category_id BIGINT,
+--     role_id BIGINT,
+--     role_name TEXT,
+--     role_needed BOOL DEFAULT FALSE,
+--     default_name TEXT,
+--     role_not_found_message TEXT,
+--     PRIMARY KEY (guild_id, category_id, role_id)
+-- );
+-- alter table lobby_category_rank_roles add role_needed BOOL DEFAULT FALSE;
+
+-- CREATE TABLE IF NOT EXISTS lobby_text_channel_ids (
+--     guild_id BIGINT REFERENCES guild(id),
+--     lobby_category_id BIGINT,
+--     text_channel_id BIGINT,
+--     PRIMARY KEY (guild_id, lobby_category_id)
+-- );
+
+
+
+-- CREATE TABLE IF NOT EXISTS lobby_messages (
+--     guild_id BIGINT REFERENCES guild(id),
+--     message_id BIGINT,
+--     voice_channel_id BIGINT,
+--     PRIMARY KEY (guild_id, message_id)
+-- );
+
+-- CREATE TABLE IF NOT EXISTS lobby_created_voice_channels_ids (
+--     guild_id BIGINT REFERENCES guild(id),
+--     channel_creator_category_id BIGINT,
+--     lobby_category_id BIGINT,
+--     PRIMARY KEY (guild_id, channel_creator_category_id)
+-- );
 
 -- endregion
 
-CREATE TABLE IF NOT EXISTS tickets (
-    guild_id BIGINT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS ticket (
+    guild_id BIGINT REFERENCES guild(id) PRIMARY KEY,
     button_cooldown INT,
-    tickets_category_id BIGINT,
-    closed_tickets_category_id BIGINT,
-    question_roles_ids BIGINT[],
-    report_roles_ids BIGINT[],
-    offer_roles_ids BIGINT[],
+    category_id BIGINT,
+    closed_ticket_category_id BIGINT,
     logs_channel_id BIGINT,
-    total_created_tickets_number INT DEFAULT 0
+    number INT DEFAULT 0
 );
 
-CREATE TABLE IF NOT EXISTS ticket_users_button_cooldown (
-    guild_id BIGINT,
-    user_id BIGINT,
-    button_cooldown_end_time TIMESTAMP WITH TIME ZONE,
+CREATE TABLE IF NOT EXISTS ticket_roles (
+    guild_id BIGINT REFERENCES guild(id) PRIMARY KEY,
+    question_roles_ids BIGINT[],
+    report_roles_ids BIGINT[],
+    offer_roles_ids BIGINT[]
+);
+
+CREATE TABLE IF NOT EXISTS ticket_user_button_cooldown (
+    guild_id BIGINT REFERENCES guild(id),
+    user_id BIGINT REFERENCES guild_member(id),
+    end_time TIMESTAMP WITH TIME ZONE,
     PRIMARY KEY (guild_id, user_id)
 );
 
-CREATE TABLE IF NOT EXISTS ticket_buttons_emojis (
-    guild_id BIGINT PRIMARY KEY,
-    question_button_emoji TEXT,
-    report_button_emoji TEXT,
-    offer_button_emoji TEXT,
-    close_button_emoji TEXT,
-    delete_button_emoji TEXT
+CREATE TABLE IF NOT EXISTS ticket_button_emojis (
+    guild_id BIGINT REFERENCES guild(id) PRIMARY KEY,
+    question_button TEXT,
+    report_button TEXT,
+    offer_button TEXT,
+    close_button TEXT,
+    delete_button TEXT
 );
 
 
